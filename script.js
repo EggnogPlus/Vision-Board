1f016e1ffd44461f031af057369a218f0f4790b3
EggnogPlus <54519955+EggnogPlus@users.noreply.github.com>
2021-07-05 00:17:31 +0000

    uploaded base code from brackets (just unfinished login)

---
+++ script.js
@@ -0,0 +1,11 @@
+//something to do with ->document.getElementById("user_div").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
+
+
+function login() {
+   var userEmail = document.getElementById("email_field").value;
+   var userPassword = document.getElementById("password_field").value;
+    
+    
+    
+}
